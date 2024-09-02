16254f7f2e62a1082d44dc21f74cf87e37610495
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-09-02 12:06:51 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.println(" digite quantos numeros seram somados ");
+  int N = sc.nextInt();
+  int soma = 0;
+    for( int i = N; i > 0; i--){
+      System.out.println(" numero a ser somado ");
+        int y = sc.nextInt();
+      soma += y;
+    }
+    System.out.println("a soma dos numeros é: * + soma);
+
+
+
+    
+    // fazer um programa que lê um valor inteiro N e depois N numeros inteiros. Ao final, mostra a soma dos N numeros lidos
+
+   // for(int i = 0; i < 5;i++ ) {
+  // System.out.println(i);
+  //  }
+
+   // for(int j = 4; j >= 0; j--){
+   //   System.out.println(j);
+  //  }
+
+    // for inicio ; condição ; incremento ) {
+    // comando
+    // }
+    
+   //   sc.close();
+//  }
+// }
